1d6b5ed3b4ee51d48f438cbf215fb62307e439fd
MarioRozic <rozic.mario@hotmail.com>
2021-04-18 13:30:10 +0200

    :sparkles: Setup project, add routes, basic components, remove unnecessary files!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from "react-router";
+import PostDetails from "./components/PostDetails";
+import PostsList from "./components/PostsList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/posts">
+          <PostsList />
+        </Route>
+        <Route path="/post-details/:id">
+          <PostDetails />
+        </Route>
+        <Redirect from="/" to="/posts" />
+      </Switch>
+    </>
   );
 }
 
